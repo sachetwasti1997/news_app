165f38fcb4025905d0a79146e6c6d56ceff06646
sachetwasti1997 <sachetwasti61@gmail.com>
2022-07-09 13:41:25 +0530

    Created use cases for gettingnewsheadlines, saving, displaying saved news, deleting saved news

--- app/src/main/java/com/sachet/news_client/domain/usecase/GetSearchedNewsUseCase.kt
+++ app/src/main/java/com/sachet/news_client/domain/usecase/GetSearchedNewsUseCase.kt
@@ -2,5 +2,5 @@ package com.sachet.news_client.domain.usecase
 
 import com.sachet.news_client.domain.repository.NewsRepository
 
-class GetSavedNewsUseCase(private val newsRepository: NewsRepository) {
+class GetSearchedNewsUseCase(private val newsRepository: NewsRepository) {
 }
